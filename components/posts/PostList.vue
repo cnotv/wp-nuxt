b1135f1e55b0187bb6f58eb70858aa1eaed448e1
cnotv <contact@cnotv.xyz>
2017-11-20 20:01:49 +0100

    initial commit

---
+++ components/posts/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="post-list">
+    <li v-for="post in posts" :key="post.id" class="post-list-item">
+      <PostExcerpt :post="post" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import PostExcerpt from './PostExcerpt.vue'
+
+export default {
+  props: {
+    posts: { type: Array }
+  },
+  components: {
+    PostExcerpt
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  padding: 0;
+}
+
+.post-list-item {
+  list-style: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="container">
+    <div>
+      <PostList :posts="posts" />
+    </div>
+  </main>
+</template>
+
+<script>
+import PostList from '~/components/posts/PostList.vue'
+import wp from '~/lib/wp'
+
+export default {
+  async asyncData ({ params }) {
+    return wp.posts()
+  },
+  head () {
+    return {
+      title: `${this.$store.state.site_data.name} | Home`
+    }
+  },
+  components: {
+    PostList
+  }
+}
+</script>
